64c8d77f49a00f79f3dc9d263b4972f6cac8f4ff
tcallred <taylor.allred21@gmail.com>
2017-10-15 03:24:23 +0000

    The site works with basic functionality

---
+++ picture_maker.js
@@ -0,0 +1,18 @@
+angular.module('app', [])
+    .controller('mainCtrl', mainCtrl);
+
+function mainCtrl($scope) {
+    $scope.images = [];
+
+    $scope.addImage = function(img) {
+        $scope.images.push({
+            picName: img.name,
+            picUrl: img.url,
+            picWidth: 500,
+            picIndex: 1
+        });
+        img.name = '';
+        img.url = '';
+        console.log($scope.images);
+    }
+}
